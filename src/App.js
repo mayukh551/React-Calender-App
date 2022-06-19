af6b9acaf3adced6fc4cc0354f70dd2520f2787e
mayukh551 <may14ukh@gmail.com>
2022-06-19 17:36:21 +0530

    Grid added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import Grid from "./Components/Grid";
+
 function App() {
-  return (
-    <div className="h-screen bg-black text-white text-4xl flex justify-center items-center">
-      Hey there
-    </div>
-  );
+	return (
+		<div className="h-screen overflow-hidden">
+			<div className="h-28 bg-slate-600 text-white text-4xl">Hi there</div>
+			<Grid />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Components/Grid.jsx
@@ -0,0 +1,34 @@
+const Grid = () => {
+    const timeSlots = [];
+
+    const days = [];
+
+    for (let i = 1; i <= 16; i++) {
+        if (i <= 7) days.push(i);
+
+        timeSlots.push(i);
+    }
+
+    return (
+        <div className="h-full overflow-y-scroll">
+            {timeSlots.map((e1) => {
+                return (
+                    <div className="w-full flex flex-grow" key={e1}>
+                        {days.map((el) => {
+                            return (
+                                <div
+                                    className="py-3 px-2 border-[1px] border-gray-900 h-28 inline-block text-center bg-orange-400"
+                                    key={el}
+                                >
+                                    Lorem ipsum dolor sit amet.
+                                </div>
+                            );
+                        })}
+                    </div>
+                );
+            })}
+        </div>
+    );
+};
+
+export default Grid;
