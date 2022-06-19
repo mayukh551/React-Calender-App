6a8376c9686567c5ed50776398cff8e1c987e611
mayukh551 <may14ukh@gmail.com>
2022-06-19 17:40:04 +0530

    timeline added

--- src/Components/Grid.jsx
+++ src/Components/Grid.jsx
@@ -1,3 +1,5 @@
+import Timeline from "./Timeline";
+
 const Grid = () => {
     const timeSlots = [];
 
@@ -11,22 +13,27 @@ const Grid = () => {
 
     return (
         <div className="h-full overflow-y-scroll">
-            {timeSlots.map((e1) => {
-                return (
-                    <div className="w-full flex flex-grow" key={e1}>
-                        {days.map((el) => {
-                            return (
-                                <div
-                                    className="py-3 px-2 border-[1px] border-gray-900 h-28 inline-block text-center bg-orange-400"
-                                    key={el}
-                                >
-                                    Lorem ipsum dolor sit amet.
-                                </div>
-                            );
-                        })}
-                    </div>
-                );
-            })}
+            <div className="flex">
+                <Timeline />
+                <div>
+                    {timeSlots.map((e1) => {
+                        return (
+                            <div className="w-full flex flex-grow" key={e1}>
+                                {days.map((el) => {
+                                    return (
+                                        <div
+                                            className="py-3 px-2 border-[1px] border-gray-900 h-28 inline-block text-center bg-orange-400"
+                                            key={el}
+                                        >
+                                            Lorem ipsum dolor sit amet.
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
         </div>
     );
 };
---
+++ src/Components/Timeline.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Timeline = () => {
+  return (
+    <div className='h-full w-[15%] text-center'>Timeline</div>
+  )
+}
+
+export default Timeline
