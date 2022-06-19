884adec33eb943b17f5eaa9f7d63a150a30d5877
mayukh551 <may14ukh@gmail.com>
2022-06-19 18:07:54 +0530

    timeline added

--- src/Components/Grid.jsx
+++ src/Components/Grid.jsx
@@ -1,31 +1,35 @@
 import Timeline from "./Timeline";
 
 const Grid = () => {
+    // const timeSlots = ['9:30 am', '10:30 am', '11:30 am', '12:30 pm'];
     const timeSlots = [];
 
     const days = [];
 
-    for (let i = 1; i <= 16; i++) {
-        if (i <= 7) days.push(i);
+    for (let i = 9; i <= 23; i++) {
+        timeSlots.push(`${i}:${30} hrs`);
+    }
 
-        timeSlots.push(i);
+    for (let i = 1; i <= 7; i++) {
+        if (i <= 7) days.push(i);
     }
 
     return (
-        <div className="h-full overflow-y-scroll">
+        <div className="h-full overflow-y-scroll pt-5">
             <div className="flex">
-                <Timeline />
-                <div>
+                <Timeline timeSlots={timeSlots} />
+                <div className="w-full">
                     {timeSlots.map((e1) => {
                         return (
+                            // for every time slot, display 7 days
                             <div className="w-full flex flex-grow" key={e1}>
                                 {days.map((el) => {
                                     return (
                                         <div
-                                            className="py-3 px-2 border-[1px] border-gray-900 h-28 inline-block text-center bg-orange-400"
+                                            className="py-3 px-2 border-[0.1px] border-gray-900 h-28 w-[14.28%] inline-block text-center"
                                             key={el}
                                         >
-                                            Lorem ipsum dolor sit amet.
+                                            {/* Lorem ipsum dolor sit amet. */}
                                         </div>
                                     );
                                 })}
--- src/Components/Timeline.jsx
+++ src/Components/Timeline.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
-const Timeline = () => {
-  return (
-    <div className='h-full w-[15%] text-center'>Timeline</div>
-  )
-}
+const Timeline = ({ timeSlots }) => {
+    return (
+        <div className="h-full w-[15%] text-center">
+            {timeSlots.map((el) => {
+                console.log(el);
+                return (
+                    <div
+                        className={`${
+                            el === "9:30 hrs" ? "h-[122px]" : "h-28"
+                        } flex flex-col justify-end`}
+                    >
+                        {el}
+                    </div>
+                );
+            })}
+        </div>
+    );
+};
 
-export default Timeline
+export default Timeline;
